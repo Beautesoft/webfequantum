2f886d7d34a21e6e18b4cce5a000e8708647dbd1
Yoonus <yoonus@sequoia-asia.com>
2022-08-11 14:32:13 +0800

    first commit

---
+++ pages/Settings/index.js
@@ -0,0 +1,10 @@
+export { SmtpSettings } from "component/Admin/Settings/SmtpSettings";
+export { SmtpCreate } from "component/Admin/Settings/SmtpSettings/SmtpCreate";
+export { SmtpDetails } from "component/Admin/Settings/SmtpSettings/SmtpDetails";
+
+export { SetupTransaction } from "component/Admin/Settings/SetupTransaction";
+export { SetupTransCreate } from "component/Admin/Settings/SetupTransaction/SetupTransCreate";
+export { SetupTransDetails } from "component/Admin/Settings/SetupTransaction/SetupTransDetails";
+
+export { TermsCondition } from "component/Admin/Settings/TermsCondition";
+
